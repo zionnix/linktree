327bf308dfb3d27a6485c2ffd72901ae470796f5
zionnix <czionnix@gmail.com>
2025-12-31 14:19:54 +0100

    correctif

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const api = axios.create({ baseURL: 'http://localhost:3000/api' });
+const api = axios.create({ baseURL: 'https://linktree-k2t6.onrender.com/api' });
 
 api.interceptors.request.use(config => {
     const token = localStorage.getItem('token');
